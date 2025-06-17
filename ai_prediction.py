0a65454e3c22397d0e803f245bd12b54640da825
roshithpk <166591536+roshithpk@users.noreply.github.com>
2025-06-17 16:02:02 +0530

    Update ai_prediction.py

--- ai_prediction.py
+++ ai_prediction.py
@@ -25,7 +25,7 @@ def add_technical_indicators(df):
     # Momentum Indicators
     st.write("Asadad")
     df['RSI'] = RSIIndicator(close=df['Close'], window=14).rsi()
-     st.write("Asadad")
+    st.write("Asadad")
     stoch = StochasticOscillator(high=df['High'], low=df['Low'], close=df['Close'], window=14)
     df['Stoch_%K'] = stoch.stoch()
     df['Stoch_%D'] = stoch.stoch_signal()
