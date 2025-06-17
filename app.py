82370209d6296c42ad1621cb5bd5119463a40e5a
roshithpk <166591536+roshithpk@users.noreply.github.com>
2025-06-17 14:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import yfinance as yf
 from ta.momentum import RSIIndicator
 from ta.trend import EMAIndicator
-from keras.models import Sequential
 import ai_prediction
 
 if "page" not in st.session_state:
